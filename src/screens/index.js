8e313471d1a2120c2ccad591eb23175d53ee9003
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-13 21:16:33 +0300

    create index.js

---
+++ src/screens/index.js
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import NotePage from "./NotePage";
+import RegisterPage from "./RegisterPage";
+import ForgotPage from "./ForgotPage";
+
+export { HomePage, LoginPage, NotePage, RegisterPage, ForgotPage };
