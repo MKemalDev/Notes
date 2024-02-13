6956962334af736c3c366608534fd72dc7213d78
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-13 21:30:44 +0300

    added userReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import userReducer from "./userSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
