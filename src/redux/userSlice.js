7de09fd43d151a1cd8688306a84a01e8c47faae2
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-13 21:33:51 +0300

    upload

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -3,16 +3,26 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   email: null,
   password: null,
-  isLoading: false,
   isAuth: false,
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    auth: (state, action) => {
+      state.email = action.payload.email;
+      state.password = action.payload.password;
+      state.isAuth = true;
+    },
+    logout: (state, action) => {
+      state.email = null;
+      state.password = null;
+      state.isAuth = false;
+    },
+  },
 });
 
-export const {} = userSlice.actions;
+export const { auth, logout } = userSlice.actions;
 
 export default userSlice.reducer;
