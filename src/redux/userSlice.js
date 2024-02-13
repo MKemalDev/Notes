af05afdcfc191bbf68698834db34481f47f05546
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-14 00:30:13 +0300

    upload

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,10 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  email: null,
-  password: null,
   isAuth: false,
+  isLoading: false,
+  user: null,
+  error: null,
 };
+
+export const login = createAsyncThunk(
+  "user/login",
+  async (username, password) => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
 
 export const userSlice = createSlice({
   name: "user",
@@ -20,6 +49,23 @@ export const userSlice = createSlice({
       state.password = null;
       state.isAuth = false;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+        state.isAuth = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = false;
+        state.error = action.error.message;
+      });
   },
 });
 
