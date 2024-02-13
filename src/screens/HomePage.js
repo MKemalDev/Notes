8767d5166c0bddf86d777eebcd3f295810b663fe
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-14 01:59:48 +0300

    upload

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
   cart: {
     width: "100%",
     height: 200,
-    backgroundColor: "#E0D9C7",
+    backgroundColor: "#E8E5DE",
     borderRadius: 10,
     padding: 20,
     gap: 20,
