306ad204bb3f3c4eca6a1c188e30fdf63317bede
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-14 17:21:27 +0300

    upload

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const initialState = {
   isAuth: false,
   isLoading: false,
-  user: null,
+  response: null,
   error: null,
 };
 
@@ -18,6 +18,28 @@ export const login = createAsyncThunk("user/login", async (formValue) => {
   formData.append("password", formValue.password);
   const response = await axios
     .post("http://192.168.1.8/note_backend/login", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw new Error(err.response.data.message);
+    });
+});
+
+export const register = createAsyncThunk("user/register", async (formValue) => {
+  if (!formValue.username || !formValue.password || !formValue.email) {
+    throw new Error("Lütfen alanları bos bırakmayınız.");
+  }
+  var formData = new FormData();
+  formData.append("username", formValue.username);
+  formData.append("password", formValue.password);
+  formData.append("email", formValue.email);
+  axios
+    .post("http://192.168.1.8/note_backend/register", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -54,9 +76,24 @@ export const userSlice = createSlice({
       .addCase(login.fulfilled, (state, action) => {
         state.isLoading = false;
         state.isAuth = false;
-        state.user = action.payload;
+        state.response = action.payload;
       })
       .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = false;
+        state.error = action.error.message;
+      });
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+        state.isAuth = false;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = false;
+        state.response = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
         state.isLoading = false;
         state.isAuth = false;
         state.error = action.error.message;
